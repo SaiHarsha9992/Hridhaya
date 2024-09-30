e14c95b5152b9bf685070d9eb6d7c0324cfa2377
Gadu Kedarinadh Sai Harsha <119718764+SaiHarsha9992@users.noreply.github.com>
2024-09-30 20:41:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/SignUp";
 import Forgot from "./pages/Forgot";
